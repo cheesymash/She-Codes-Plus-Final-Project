8f8758b0175a2a6da2e6da188521f7f7c54ca85e
Linz <149715586+cheesymash@users.noreply.github.com>
2024-03-20 14:26:32 +0000

    added api key

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+function searchCity(city) {
+  let apiKey = "5034aee8be80fd426tb5da773ac1o38a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=mertic`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
