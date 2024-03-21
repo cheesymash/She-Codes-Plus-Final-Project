4c89964617c764c346da46965b5db7d39c9432ae
Linz <149715586+cheesymash@users.noreply.github.com>
2024-03-21 10:59:08 +0000

    added weather details "scattered clouds" to html

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ function refreshWeather(responce) {
   cityElement.innerHTML = responce.data.city;
 
   temperatureElement.innerHTML = Math.round(temperature);
-}
+} //changes temperature innerhtml
 
 function searchCity(city) {
   let apiKey = "5034aee8be80fd426tb5da773ac1o38a";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
-}
+} //fetch api key
 
 function handleSearchSubmit(event) {
   event.preventDefault();
@@ -19,9 +19,10 @@ function handleSearchSubmit(event) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
-}
+} //changes city innerhtml using search
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+//function searchform- addevent listener
 
-searchCity("London");
+searchCity("London"); //automatically allows city to be london when refreshed
