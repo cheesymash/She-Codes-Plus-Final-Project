c028af6c643f46b4a9e8a7da258c90af72b6c3d7
Linz <149715586+cheesymash@users.noreply.github.com>
2024-03-21 11:45:42 +0000

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,38 @@ function refreshWeather(responce) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = responce.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#date-time");
+  let date = new Date(responce.data.time * 1000);
+
   cityElement.innerHTML = responce.data.city;
-
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = responce.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
-} //changes temperature innerhtml
+  humidityElement.innerHTML = `${responce.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${Math.round(responce.data.wind.speed)}km/h`;
+} //changes temperature and conditions innerhtml
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
+}
 function searchCity(city) {
   let apiKey = "5034aee8be80fd426tb5da773ac1o38a";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
